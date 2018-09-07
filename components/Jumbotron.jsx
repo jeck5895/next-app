2d3b25992182ae96ef912a9360e147969a70c855
phpdeveloper12 <phpdeveloper12@unilab.com.ph>
2018-09-07 14:05:14 +0800

    Added design/Jumbotron

---
+++ components/Jumbotron.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Jumbotron = () => (
+    <section className="hero is-info is-large" style={{ backgroundImage: 'url("https://previews.123rf.com/images/floralset/floralset1605/floralset160500012/56192716-programming-coding-flat-computing-background-code-hardware-software-web-development-search-engine-op.jpg")' }}>
+        <div className="hero-body">
+            <div className="container">
+                <h1 className="title">
+                    Large title
+                </h1>
+                <h2 className="subtitle">
+                    Large subtitle
+                </h2>
+            </div>
+        </div>
+    </section >
+);
+
+export default Jumbotron;
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 import Head from 'next/head'
 import bulma from '../assets/styles/bulma.scss';
 
+/**
+ * Component imports
+ */
+
+import Navbar from '../components/Navbar'
+import Jumbotron from '../components/Jumbotron'
+
 class Index extends Component {
 
     render() {
@@ -9,11 +16,14 @@ class Index extends Component {
             <div>
                 <style dangerouslySetInnerHTML={{ __html: bulma }} />
                 <Head> 
-                    <title>Next App</title>
+                    <title>My Portfolio</title>
                     <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
+                    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous"></link>
+                    <link href="https://fonts.googleapis.com/css?family=IM+Fell+Great+Primer" rel="stylesheet"></link>
                 </Head>
-                <div className="container">
-                    This is home page
+                <Navbar />
+                <div className="fixed-box">
+                    <Jumbotron/>
                 </div>
             </div>
         );
